252603be12fbd7ed1c67a2f1f7719a459950c004
Carter Carlson <carlfarterson@gmail.com>
2019-05-09 22:34:42 -0500

    Rename candle directory

---
+++ strategies/candles/bullish-engulfing.py
@@ -0,0 +1,22 @@
+import pandas as pd
+import numpy as np
+
+
+df = pd.read_csv('data/btc_hourly_candle_2019.csv')
+
+
+
+
+
+'''
+Concept: Red candle (1) followed by larger green candle (2)
+1. high (1) < high (2)
+2. low (1) > low (2)
+3. open (1) < close (2)
+4. [Bottom candlestick criteria?]
+'''
+# Criteria 1
+
+# Criteria 2
+
+# Criteria 3
