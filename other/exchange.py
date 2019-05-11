5b8473899053530bba6a2e4829cd0dccc83c1034
Carter Carlson <carlfarterson@gmail.com>
2019-05-11 07:27:36 -0500

    Create additional directory for exchange methods

---
+++ other/exchange.py
@@ -0,0 +1,15 @@
+import ccxt
+import pandas as pd
+
+
+class Exchange:
+
+    api_file = 'data/api.csv'
+
+    def __init__(self, **kwargs):
+        key, secret = pd.read_csv(file).values[0]
+        
+
+
+    def get_price(self, coin):
+        pass
