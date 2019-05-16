92add4319bb3b0a69d1e7131bc83a26de23af6a3
Carter Carlson <carlfarterson@gmail.com>
2019-05-16 16:35:06 -0400

    Finish core engulfing logic

--- strategies/candles/engulfing.py
+++ strategies/candles/engulfing.py
@@ -1,42 +1,32 @@
 import pandas as pd
 import numpy as np
 
+period = 1
 
 df = pd.read_csv('data/btc_hourly_candle_2019.csv')
-df['Positive'] = df['close'] > df['open']
 
-print(df['Positive'][:10])
-print(df['Positive'].shift(1)[:10])
+# True == green candle
+df['green'] = df['close'] > df['open']
+df['max'] = map(max, df[['open', 'close']].values)
+df['min'] = map(min, df[['open', 'close']].values)
 
+candle1 = df[period:]
+candle2 = df.shift(period)[period:]
+df = df[period:]
+
+# Compare candles to only take occurences of the next candle being the opposite color
+df['switch color'] = candle1['green'] != candle2['green']
+
+# candle is larger than previous candle
+df['> previous'] = abs(candle1['open'] - candle1['close']) < abs(candle2['open'] - candle2['close'])
+
+# open/close of candle > open/close of previous candle
+engulf_upper = candle1['max'] <= candle2['max']
+engulf_lower = candle1['min'] >= candle2['min']
+df['engulfing'] = engulf_upper & engulf_lower
 
 df.head()
 
-df['test'] = df['Positive'] == df['Positive'].shift(1)
-df[['open', 'close', 'Positive', 'test']]
-df[['open', 'close', 'Positive', 'test']].dropna(axis=0)
-
-# Which columns are needed for this iteration?
-test = df[10:15]
-for i, row in test.iterrows():
-    row2 = next(row)
-
-
-
-
-
-
-
-
-'''
-Concept: Red candle (1) followed by larger green candle (2)
-Question: How do we efficiently find both bull/bear sides?
-1. high (1) < high (2)
-2. low (1) > low (2)
-3. open (1) < close (2)
-4. [Bottom candlestick criteria?]
-'''
-# Criteria 1
-
-# Criteria 2
-
-# Criteria 3
+df = df[['date', 'max', 'min', 'switch color', '> previous']]
+for i, row in df.loc[df['switch color'] == True][:20].iterrows():
+    pass
