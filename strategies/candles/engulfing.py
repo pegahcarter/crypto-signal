0f136db57a3c379edb2da0d84a8676050a3a5dd9
Carter Carlson <carlfarterson@gmail.com>
2019-05-17 03:08:26 -0400

    Add efficient engulfing logic

--- strategies/candles/engulfing.py
+++ strategies/candles/engulfing.py
@@ -15,7 +15,7 @@ candle2 = df.shift(period)[period:]
 df = df[period:]
 
 # Compare candles to only take occurences of the next candle being the opposite color
-df['switch color'] = candle1['green'] != candle2['green']
+df['switch color'] = candle2['green'] != candle1['green']
 
 # candle is larger than previous candle
 df['> previous'] = abs(candle1['open'] - candle1['close']) < abs(candle2['open'] - candle2['close'])
@@ -25,8 +25,7 @@ engulf_upper = candle1['max'] <= candle2['max']
 engulf_lower = candle1['min'] >= candle2['min']
 df['engulfing'] = engulf_upper & engulf_lower
 
-df.head()
 
-df = df[['date', 'max', 'min', 'switch color', '> previous']]
-for i, row in df.loc[df['switch color'] == True][:20].iterrows():
-    pass
+# df = df[['date', 'switch color', '> previous', 'engulfing']]
+conditions = df['switch color'] & df['> previous'] & df['engulfing']
+engulfings = conditions.where(conditions == True).dropna()
