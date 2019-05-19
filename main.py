924515ad90deb2522e87d53d9ac145c4ba0a8518
Carter Carlson <carlfarterson@gmail.com>
2019-05-19 13:20:40 -0400

    Begin main.py

---
+++ main.py
@@ -0,0 +1,20 @@
+import pandas as pd
+import strategies.candles.engulfing
+
+filename = 'data/btc_hourly_candle_2019.csv'
+
+
+def main():
+    signal_df = pd.DataFrame()
+    df = Load(filename)
+    dff.add_columns()
+    df.shift(1)
+    engulfing(df.data['df0'], df.data['df1'])
+
+
+
+
+
+
+if __name__ == "__main__":
+    main()
