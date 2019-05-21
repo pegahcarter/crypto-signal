198adbc689a7c332acf7a0d62a9218c05cc763bc
Carter Carlson <carlfarterson@gmail.com>
2019-05-21 11:20:09 -0400

    Integrate engulfing with new Load class. Still need to test

--- main.py
+++ main.py
@@ -9,12 +9,8 @@ def main():
     df = Load(filename)
     dff.add_columns()
     df.shift(1)
-    engulfing(df.data['df0'], df.data['df1'])
-
-
-
-
-
+    engulf_col = engulfing(df.data['df0'], df.data['df1'])
+    
 
 if __name__ == "__main__":
     main()
--- strategies/candles/engulfing.py
+++ strategies/candles/engulfing.py
@@ -2,33 +2,22 @@ import pandas as pd
 import numpy as np
 
 
-window = 1
 param = {
     'columns_to_keep': ['date', 'open', 'close'],
-    'functions_to_apply': [
-        'maxmin', 'color_candle'
-    ]
+    'functions_to_apply': ['maxmin', 'color_candle']
 }
 
 def engulfing(df0, df1):
+    # Compare candles to only take occurences of the next candle being the opposite color
+    switch_color = df1['green'] != df0['green']
 
+    # candle is larger than previous candle
+    gt_previous = abs(df0['open'] - df0['close']) < abs(df1['open'] - df1['close'])
 
+    # open/close of candle >= open/close of previous candle
+    engulf_upper = df0['max'] <= df1['max']
+    engulf_lower = df0['min'] >= df1['min']
+    engulfed = engulf_upper & engulf_lower
 
-candle1 = df[period:]
-candle2 = df.shift(period)[period:]
-df = df[period:]
-
-# Compare candles to only take occurences of the next candle being the opposite color
-df['switch color'] = candle2['green'] != candle1['green']
-
-# candle is larger than previous candle
-df['> previous'] = abs(candle1['open'] - candle1['close']) < abs(candle2['open'] - candle2['close'])
-
-# open/close of candle > open/close of previous candle
-engulf_upper = candle1['max'] <= candle2['max']
-engulf_lower = candle1['min'] >= candle2['min']
-df['engulfing'] = engulf_upper & engulf_lower
-# Only use criteria where candle 2 size is > 2x
-
-conditions = df['switch color'] & df['> previous'] & df['engulfing']
-engulfings = conditions.where(conditions == True).dropna()
+    # TODO: Only use criteria where candle 2 size is > 2x
+    return switch_color & gt_previous & engulfed
