653d664e48f2b6c40736a880332129694bb6156e
Carter Carlson <carlfarterson@gmail.com>
2019-05-24 17:08:35 -0500

    Create basic candlestick chart

---
+++ graph_tests.py
@@ -0,0 +1,12 @@
+import plotly.graph_objs as go
+from plotly.offline import download_plotlyjs, init_notebook_mode, plot, iplot
+import pandas as pd
+
+filename = 'data/btc_hourly_candle_2019.csv'
+
+df = pd.read_csv(filename)[:50]
+trace = go.Candlestick(x = df['date'], open=df['open'], high=df['high'], low=df['low'], close=df['close'])
+data = [trace]
+
+
+iplot(data, filename='simple_candlestick_without_range_slider')
