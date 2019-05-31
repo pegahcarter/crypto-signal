3af8fb4a951044c16de77a8bc0e01dcd275797a7
Carter Carlson <carlfarterson@gmail.com>
2019-05-31 01:11:41 -0500

    Add remaining single candle indicators

--- strategies/candles/all_patterns.py
+++ strategies/candles/all_patterns.py
@@ -19,3 +19,13 @@ df['bearish'] = df['open'] < df['close']
 
 df['hammer'] = df['tail_length'] > df['body_length']*2 & df['wick_length'] < df['body_length']
 df['inverted_hammer'] = df['wick_length'] > df['body_length']*2 & df['tail_length'] < df['body_length']
+
+df['bullish_hammer'] = df['bullish'] & df['hammer']
+df['bearish_inverted_hammer'] = df['bearish'] & df['inverted_hammer']
+
+# True if candle is larger than previous candle
+df['engulfing'] = df['body_length'].shift(1) > df['body_length']
+
+# Determine if there's a space between the last close/next open
+df['gap_up'] = df['body_max'] < df['body_min'].shift(1)
+df['gap_down'] = df['body_min'] > df['body_max'].shift(1)
