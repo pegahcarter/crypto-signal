921f79fe6d590eba004bca0a090a9620bf7ef444
Carter Carlson <carlfarterson@gmail.com>
2019-05-31 01:27:22 -0500

    Add some dope 2-candle indicators

--- strategies/candles/all_patterns.py
+++ strategies/candles/all_patterns.py
@@ -14,6 +14,9 @@ df['body_length'] = abs(df['open'] - df['close'])
 df['wick_length'] = df['high'] - df['body_max']
 df['tail_length'] = df['body_min'] - df['low']
 
+# -----------------------------------------------------------------------
+# Single candle calculations
+
 df['bullish'] = df['close'] > df['open']
 df['bearish'] = df['open'] < df['close']
 
@@ -29,3 +32,22 @@ df['engulfing'] = df['body_length'].shift(1) > df['body_length']
 # Determine if there's a space between the last close/next open
 df['gap_up'] = df['body_max'] < df['body_min'].shift(1)
 df['gap_down'] = df['body_min'] > df['body_max'].shift(1)
+
+# -----------------------------------------------------------------------
+# Double candle calculations
+bull = pd.DataFrame()
+bear = pd.DataFrame()
+
+# Candles that can be either bull/bear
+bull['engulfed'] = df['bearish'] & df['bullish'].shift(1) & df['engulfed']
+bear['engulfed'] = df['bullish'] & df['bearish'].shift(1) & df['engulfed']
+
+bull['harami'] = df['bearish'] & df['bullish'].shift(1) & df['engulfed']
+bear['harami'] = df['bullish'] & df['bearish'].shift(1) & df['engulfed']
+
+bull['kicker'] = df['bearish'] & df['bullish'].shift(1) & df['gap_up']
+bear['kicker'] = df['bullish'] & df['bearish'].shift(1) & df['gap_down']
+
+# Candles specific to bull/bear
+bear['hanging_man'] = df['bullish'] & df['bearish'].shift(1) & df['gap_up'] & df['hammer']
+bear['shooting_star'] = df['bullish'] & df['bearish'].shift(1) & df['gap_up'] & df['inverted_hammer'].shift(1)
