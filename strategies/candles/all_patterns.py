61538b9bb9ebf514f610550a822b026a3ac8c720
Carter Carlson <carlfarterson@gmail.com>
2019-06-01 11:18:23 -0500

    Add engulfed function and simplify names x2

--- strategies/candles/all_patterns.py
+++ strategies/candles/all_patterns.py
@@ -1,63 +1,68 @@
-# File that references candlestick functions from https://github.com/cm45t3r/candlestick/blob/master/src/candlestick.js
 import pandas as pd
 
-candle = pd.read_csv('data/btc_hourly_candle_2019.csv')
+df = pd.read_csv('data/btc_hourly_c_2019.csv')
 
-candle_bodies = candle[['open', 'close']].values
-candle['body_max'] = list(map(max, candle_bodies))
-candle['body_min'] = list(map(min, candle_bodies))
+candle_bodies = df[['open', 'close']].values
+df['body_max'] = list(map(max, candle_bodies))
+df['body_min'] = list(map(min, candle_bodies))
 
-candle['body_length'] = abs(candle['open'] - candle['close'])
-candle['wick_length'] = candle['high'] - candle['body_max']
-candle['tail_length'] = candle['body_min'] - candle['low']
+df['body_length'] = abs(df['open'] - df['close'])
+df['wick_length'] = df['high'] - df['body_max']
+df['tail_length'] = df['body_min'] - df['low']
 
 # -----------------------------------------------------------------------
 # Single candle calculations
 
-candle['bullish'] = candle['close'] > candle['open']
-candle['bearish'] = candle['open'] < candle['close']
+df['+'] = df['close'] > df['open']
+df['-'] = df['open'] < df['close']
 
-candle['hammer'] = candle['bullish'] candle['tail_length'] > candle['body_length']*2 & candle['wick_length'] < candle['body_length']
-candle['inverted_hammer'] = candle['bearish'] & candle['wick_length'] > candle['body_length']*2 & candle['tail_length'] < candle['body_length']
+df['hammer'] = df['+'] df['tail_length'] > df['body_length']*2 & df['wick_length'] < df['body_length']
+df['inverted_hammer'] = df['-'] & df['wick_length'] > df['body_length']*2 & df['tail_length'] < df['body_length']
 
-candle['bullish_hammer'] = candle['bullish'] & candle['hammer']
-candle['bearish_inverted_hammer'] = candle['bearish'] & candle['inverted_hammer']
-
-# True if candle is larger than previous candle
-candle['engulfing'] = candle['body_length'].shift(1) > candle['body_length']
+df['bullish_hammer'] = df['+'] & df['hammer']
+df['bearish_inverted_hammer'] = df['-'] & df['inverted_hammer']
 
 # Determine if there's a space between the last close/next open
-candle['gap_up'] = candle['body_min'].shift(1) > candle['body_max']
-candle['gap_down'] = candle['body_max'].shift(1) < candle['body_min']
+df['gap_up'] = df['body_min'].shift(1) > df['body_max']
+df['gap_down'] = df['body_max'].shift(1) < df['body_min']
+
+# -----------------------------------------------------------------------
+# Functions
+def engulfed(smaller, larger):
+    return smaller['body_length'] < larger['body_length']
+
+
 
 # -----------------------------------------------------------------------
 
-candle_2 = candle.shift(1)[2:]
-candle_3 = candle.shift(2)[2:]
-candle = candle[2:]
+c_1 = df[2:]
+c_2 = df.shift(1)[2:]
+c_3 = df.shift(2)[2:]
+
+candle = c_1[2:]
 
 bull = pd.DataFrame()
 bear = pd.DataFrame()
 
 # -----------------------------------------------------------------------
-
 # Bear & Bull candles
-bull['engulfed'] = candle['bearish'] & candle_2['bullish'] & candle['engulfed']
-bear['engulfed'] = candle['bullish'] & candle_2['bearish'] & candle['engulfed']
 
-bull['harami'] = candle['bearish'] & candle_2['bullish'] & candle['engulfed']
-bear['harami'] = candle['bullish'] & candle_2['bearish'] & candle['engulfed']
+bull['engulfed'] = engulfed(c_1, c_2) & c_1['-'] & c_2['+']
+bear['engulfed'] = engulfed(c_1, c_2) & c_1['+'] & c_2['-']
 
-bull['kicker'] = candle['bearish'] & candle_2['bullish'] & candle['gap_up']
-bear['kicker'] = candle['bullish'] & candle_2['bearish'] & candle['gap_down']
+bull['harami'] = engulfed(c_2, c_1) & c_1['-'] & c_2['+']
+bear['harami'] = engulfed(c_2, c_1) & c_1['+'] & c_2['-']
 
-bull['tweezer'] = candle['bearish'] & candle_2['bullish']
-bear['tweezer'] = candle['bullish'] & candle_2['bearish']
+bull['kicker'] = c_1['-'] & c_2['+'] & c_1['gap_up']
+bear['kicker'] = c_1['+'] & c_2['-'] & c_1['gap_down']
+
+bull['tweezer'] = c_1['-'] & c_2['+']
+bear['tweezer'] = c_1['+'] & c_2['-']
 
 
 # Bear-specfic candles
-bear['hanging_man'] = candle['bullish'] & candle_2['bearish'] & candle['gap_up'] & candle['hammer']
-bear['shooting_star'] = candle['bullish'] & candle_2['bearish'] & candle['gap_up'] & candle_2['inverted_hammer']
+bear['hanging_man'] = c_1['+'] & c_2['-'] & c_1['gap_up'] & c_1['hammer']
+bear['shooting_star'] = c_1['+'] & c_2['-'] & c_1['gap_up'] & c_2['inverted_hammer']
 
 
 # Bull-specfic candles
