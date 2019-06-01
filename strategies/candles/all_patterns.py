bb3915918ac9a4809433cecd9feeedae27e8d1a8
Carter Carlson <carlfarterson@gmail.com>
2019-06-01 12:15:43 -0500

    Add gap/tweezer functions

--- strategies/candles/all_patterns.py
+++ strategies/candles/all_patterns.py
@@ -2,47 +2,49 @@ import pandas as pd
 
 df = pd.read_csv('data/btc_hourly_c_2019.csv')
 
-candle_bodies = df[['open', 'close']].values
-df['body_max'] = list(map(max, candle_bodies))
-df['body_min'] = list(map(min, candle_bodies))
-
-df['body_length'] = abs(df['open'] - df['close'])
-df['wick_length'] = df['high'] - df['body_max']
-df['tail_length'] = df['body_min'] - df['low']
-
-# -----------------------------------------------------------------------
-# Single candle calculations
-
 df['+'] = df['close'] > df['open']
 df['-'] = df['open'] < df['close']
 
-df['hammer'] = df['+'] df['tail_length'] > df['body_length']*2 & df['wick_length'] < df['body_length']
-df['inverted_hammer'] = df['-'] & df['wick_length'] > df['body_length']*2 & df['tail_length'] < df['body_length']
+df['body'] = abs(df['open'] - df['close'])
+df['wick'] = df['high'] - df['body_max']
+df['tail'] = df['body_min'] - df['low']
 
-df['bullish_hammer'] = df['+'] & df['hammer']
-df['bearish_inverted_hammer'] = df['-'] & df['inverted_hammer']
+df['body_max'] = list(map(max, df[['open', 'close']].values))
+df['body_min'] = list(map(min, df[['open', 'close']].values))
 
-# Determine if there's a space between the last close/next open
-df['gap_up'] = df['body_min'].shift(1) > df['body_max']
-df['gap_down'] = df['body_max'].shift(1) < df['body_min']
+df['doji'] = df['body'] < 0.1 * (df['wick'] + df['tail'])
+
+# TODO: better param?
+df['hammer'] = df['tail'] > 3 * df['body']
+df['inverted_hammer'] = df['wick'] > 3 * df['body']
 
 # -----------------------------------------------------------------------
 # Functions
+
 def engulfed(smaller, larger):
-    return smaller['body_length'] < larger['body_length']
+    return smaller['body'] < larger['body']
 
+def gap(up_down, first, second):
+    if up_down == 'up':
+        return first['body_max'] < second['body_min']
+    else:  # up_down == 'down'
+        return first['body_min'] > second['body_max']
 
+def tweezer(top_bottom, first, second):
+    common_criteria = engulfed(first, second) & first['+'] != second['+']
+    if top_bottom == 'top':
+        return common_criteria & first['body_max'] > second['wick']
+    else:  # top_bottom == 'down'
+        return common_criteria & first['body_min'] < second['tail']
 
 # -----------------------------------------------------------------------
+
+bull = pd.DataFrame()
+bear = pd.DataFrame()
 
 c_1 = df[2:]
 c_2 = df.shift(1)[2:]
 c_3 = df.shift(2)[2:]
-
-candle = c_1[2:]
-
-bull = pd.DataFrame()
-bear = pd.DataFrame()
 
 # -----------------------------------------------------------------------
 # Bear & Bull candles
@@ -53,9 +55,10 @@ bear['engulfed'] = engulfed(c_1, c_2) & c_1['+'] & c_2['-']
 bull['harami'] = engulfed(c_2, c_1) & c_1['-'] & c_2['+']
 bear['harami'] = engulfed(c_2, c_1) & c_1['+'] & c_2['-']
 
-bull['kicker'] = c_1['-'] & c_2['+'] & c_1['gap_up']
-bear['kicker'] = c_1['+'] & c_2['-'] & c_1['gap_down']
+bull['kicker'] = gap('up', c_1, c_2) & c_1['-'] & c_2['+']
+bear['kicker'] = gap('down', c_1, c_2) & c_1['+'] & c_2['-']
 
+# TODO
 bull['tweezer'] = c_1['-'] & c_2['+']
 bear['tweezer'] = c_1['+'] & c_2['-']
 
